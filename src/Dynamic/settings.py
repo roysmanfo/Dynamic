c8f3926aaac8755f92467b3f0c3c63ee2ff8630d
RoysManfo <manforoys@gmail.com>
2022-08-28 00:28:15 +0200

    Changed security key to avoid security issues

--- src/Dynamic/settings.py
+++ src/Dynamic/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&omlyvbfzz%0fo$57814io5_ltc7%5*s8@3*0&2fz^@4xt8fk5'
+with open("/key.txt") as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
