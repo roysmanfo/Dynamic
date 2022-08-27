0d69646a3941e958e8b032e4317a111884f1cddf
RoysManfo <manforoys@gmail.com>
2022-08-28 01:46:54 +0200

    [Fix] Resolved problems with secret key

--- src/Dynamic/settings.py
+++ src/Dynamic/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from .key import get_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open("/key.txt") as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = get_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
