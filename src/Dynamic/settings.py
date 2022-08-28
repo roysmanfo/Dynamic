e317d30c6f2dfe43ed8ddffff862d1d48eff6a85
RoysManfo <manforoys@gmail.com>
2022-08-28 02:29:58 +0200

    [Add] principal index page

--- src/Dynamic/settings.py
+++ src/Dynamic/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'index.apps.IndexConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
