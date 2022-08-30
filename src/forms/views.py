7cd764f1e9a41247a5b55297f96b3fb555464749
RoysManfo <manforoys@gmail.com>
2022-08-30 11:53:24 +0200

    [Add] Created basic components for user auth

--- src/forms/views.py
+++ src/forms/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+def logout(request):
+    return render(request, 'logout.html')
+
+def password_change(request, user_id):
+    return render(request, 'password_change.html')
+
+def password_changed(request):
+    return render(request, 'password_change.html')
+
+def reset_password(request, user_id):
+    return render(request, 'reset_password.html')
+
+def sign_in(request):
+    return render(request, 'sign_in.html')
