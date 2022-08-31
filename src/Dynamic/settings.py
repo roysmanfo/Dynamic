5966ebc44e708b1eead0985101d7d8fca5cd9825
RoysManfo <manforoys@gmail.com>
2022-09-01 01:07:27 +0200

    [Mod] Added redirect url on login

--- src/Dynamic/settings.py
+++ src/Dynamic/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Redirect
+
+LOGIN_REDIRECT_URL = 'home'
