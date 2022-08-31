cb2a0efabb2470fa101ba8bf30bfa057a6c0f4d8
RoysManfo <manforoys@gmail.com>
2022-08-31 20:14:35 +0200

    [Mod] Created logout view and changed some classes

--- src/forms/views.py
+++ src/forms/views.py
@@ -1,6 +1,8 @@
+from re import template
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LogoutView
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -8,16 +10,15 @@ from django.urls import reverse_lazy
 class UserRegistrationView(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('home')
 
 
 class UserLoginView(generic.CreateView):
     template_name = 'registration/login.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('home')
 
-
-def logout(request):
-    return render(request, 'logout.html')
+class UserLogoutView(generic.CreateView):
+    template_name = 'registration/logged_out.html'
 
 def password_change(request, user_id):
     return render(request, 'password_change.html')
