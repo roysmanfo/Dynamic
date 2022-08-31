36b08ffb17769ac031badf6861125f9261850a78
RoysManfo <manforoys@gmail.com>
2022-08-31 02:25:23 +0200

    [Mod] Included django default auth system

--- src/Dynamic/urls.py
+++ src/Dynamic/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
     path('', include('forms.urls')),
 ]
