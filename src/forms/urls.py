7f17e54e9f7d24308513a0729f4c252049c7ab0a
RoysManfo <manforoys@gmail.com>
2022-08-31 20:13:45 +0200

    [Mod] Added logout url

--- src/forms/urls.py
+++ src/forms/urls.py
@@ -1,12 +1,12 @@
+from re import template
 from django.urls import path
 from . import views
 
 app_name = 'forms'
 
 urlpatterns = [
-    path('login/', views.UserLoginView.as_view(), name='login'),
-    # path('login/', views.login ,name='login'),
-    path('logout/', views.logout ,name='logout'),
+    path('logout/', views.LogoutView.as_view() ,name='logout'),
+    path('login/', views.UserLoginView.as_view(), name='login'),    
     path('sign/', views.UserRegistrationView.as_view() ,name='register'),
     path('change_password/<str:user_id>/', views.password_change ,name='password_change'),
     path('password_changed/', views.password_changed ,name='password_changed'),
