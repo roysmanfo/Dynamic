442a1c67b3361fc9072d93741c301b6c1ba7ea2a
Roys Manfo <94235711+RoysManfo@users.noreply.github.com>
2022-09-02 00:06:36 +0200

    [Mod] Removing forms app
    It ain't working, will be reintroduced

--- src/Dynamic/settings.py
+++ src/Dynamic/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'index.apps.IndexConfig',
-    'forms.apps.FormsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
