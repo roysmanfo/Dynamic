048e274cc5d8af55f2289bfa455eabea731a9d44
RoysManfo <manforoys@gmail.com>
2022-09-01 23:16:06 +0200

    [Mod] Big change: added comma

--- src/Dynamic/urls.py
+++ src/Dynamic/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
     path('', include('forms.urls')),
-    path('', include('index.urls'))
+    path('', include('index.urls')),
 ]
