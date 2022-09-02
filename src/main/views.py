70b9c9333b862c2725ff10fe7be5210b6c5c7a5a
RoysManfo <manforoys@gmail.com>
2022-09-02 02:40:31 +0200

    [Mod] Modified app name to main

--- src/main/views.py
+++ src/main/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
+def home(request):
     return render(request, 'index.html')
