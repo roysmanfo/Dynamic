24a9b5a0e80dd6ed0689dd66d6471f12457df85e
RoysManfo <manforoys@gmail.com>
2022-09-03 14:50:51 +0200

    [Mod] Created UserDetails model

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserDetails(models.Models):
+    """
+    UserDetails is an extension of the django.contrib.auth.models.User model.\n
+    Contains additional information for the User model
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField("profile picture")
