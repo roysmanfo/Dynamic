c41d16def2e75340047fd1b3eaa87fdf43892376
RoysManfo <manforoys@gmail.com>
2022-09-07 02:28:28 +0200

    [Mod] Created models

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import UserDetail, TaskList, Task
 # Register your models here.
+admin.site.register(UserDetail)
+admin.site.register(TaskList)
+admin.site.register(Task)
