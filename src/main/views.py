eb68066a3b0ad548754dad1cfdedf45a7a499dec
RoysManfo <manforoys@gmail.com>
2022-09-07 19:17:43 +0200

    [Mod] Style for tasks and tasklists

--- src/main/views.py
+++ src/main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TaskList, Task
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    context = {
+        'Lists':TaskList.objects.filter(user=request.user),
+        'Tasks':Task.objects.filter(user=request.user),
+        }
+    return render(request, 'index.html', context)
