79f23add70cf1eefa804c909ee918eef596fe6ff
RoysManfo <manforoys@gmail.com>
2022-09-11 18:41:04 +0200

    [Add] CreateYask and CreateList forms

---
+++ src/main/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Task, TaskList
+class CreateTask(ModelForm):
+    class Meta():
+        model = Task
+        fields = ('title', 'text', 'parent_list', 'due')
+
+class CreateList(ModelForm):
+    class Meta():
+        model = TaskList
+        fields = ('name', 'color')
+        
